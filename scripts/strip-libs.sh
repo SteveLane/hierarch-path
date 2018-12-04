483cc98217f95f00a2a1ba2caa634cf41057caf9
Steve Lane <lane.s@unimelb.edu.au>
2018-12-04 12:06:52 +1100

    add strip libs

---
+++ scripts/strip-libs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Strips libraries from .R/.r scripts and creates a text file with a list of them.
+# Requires first argument as folder where scripts are stored, and second as the
+# text file it is stored in.
+folder=$1
+inst=$2
+for filename in $folder/*.{r,R}{md,nw,}; do
+    if [ -f $filename ]; then
+	awk -F '[(]|[)]' '/^library|^require/{print $2;}' $filename >> $inst
+    fi
+done
+sort < $inst > $inst.bk
+uniq < $inst.bk > $inst
+rm $inst.bk
